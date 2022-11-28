77444aa547102d585206a57ec95c2448cd5acc87
Lorenzo Brown <lrnzbr@gmail.com>
2022-11-28 11:22:31 -0600

    initial commit

---
+++ ArabicRootTranslator/ArabicRootTranslator/ArabicRootTranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicRootTranslatorApp.swift
+//  ArabicRootTranslator
+//
+//  Created by Lorenzo Brown on 11/28/22.
+//
+
+import SwiftUI
+
+@main
+struct ArabicRootTranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArabicRootTranslator/ArabicRootTranslator/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ArabicRootTranslator
+//
+//  Created by Lorenzo Brown on 11/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
